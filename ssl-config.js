8a3c99c99ba38d3b1d8db2262c564a91b51c9a04
Hayden Braxton <haydenbraxton@haydenbraxton.com>
2020-12-03 17:13:56 -0500

    i think this will work

--- ssl-config.js
+++ ssl-config.js
@@ -7,11 +7,15 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const defaultCertPath = join(__dirname, 'localhost.cert')
 const defaultKeyPath = join(__dirname, 'localhost.key')
 
-const envCertPath = process.env.SSL_CERT_PATH
-const envKeyPath = process.env.SSL_KEY_PATH
+const isProd = process.argv.includes('--prod')
 
-console.log(envCertPath)
-console.log(envKeyPath)
+let certIndex = process.argv.findIndex('--ssl-cert-path')
+let keyIndex = process.argv.findIndex('--ssl-key-path')
+const envCertPath = isProd ? process.argv[certIndex + 1] : ''
+const envKeyPath = isProd ? process.argv[keyIndex + 1] : ''
+
+// console.log(envCertPath)
+console.log(process.env.argv)
 
 export const sslConfig = {
 	cert: readFileSync(envCertPath || defaultCertPath),
